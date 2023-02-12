d55a70c5dc21c304cc6ab94d41a018a1dbf2f7b8
Taleh <taleh93@yahoo.com>
2023-02-13 01:50:40 +0400

    spin_btns

---
+++ js.js
@@ -0,0 +1,8 @@
+// let spn = document.querySelectorAll("span");
+// let btn = document.querySelectorAll("button");
+
+// btn.forEach((item) => {
+//   item.addEventListener("mousedown", () => {
+//     spn.forEach((elem) => elem.classList.add(".active"));
+//   });
+// });
